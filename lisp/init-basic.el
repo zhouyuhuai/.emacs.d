3c1c85751aff34520fad11f4c87824bd9a73f1a0
zhouyuhuai <makefunny@outlook.com>
2020-09-29 00:23:51 +0800

    Fonts

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -9,6 +9,7 @@
 ;;; Code:
 
 (require 'init-const)
+(require 'init-funcs)
 
 ;; Encoding
 ;; UTF-8 as the default coding system
@@ -136,6 +137,26 @@
 ;; Highlight paren
 (show-paren-mode t)
 
+;; Fonts
+(when (display-graphic-p)
+  ;; Set default font
+  (cl-loop for font in '("SF Mono" "Hack" "Source Code Pro" "Fira Code"
+                         "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+           when (font-installed-p font)
+           return (set-face-attribute 'default nil
+                                      :font font
+                                      :height (cond (sys/win32p 110)
+                                                    (t 100))))
+
+  ;; Specify font for all unicode characters
+  (cl-loop for font in '("Symbola" "Symbol")
+           when (font-installed-p font)
+           return(set-fontset-font t 'unicode font nil 'prepend))
+
+  ;; Specify font for Chinese characters
+  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+           when (font-installed-p font)
+           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
 
 (provide 'init-basic)
 
